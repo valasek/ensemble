03a3ee5a48e928800407873cba989527a0b9763d
Stanislav Valášek <valasek@gmail.com>
2026-02-09 15:54:55 +0100

    fix

--- app/models/member_of_assembly.rb
+++ app/models/member_of_assembly.rb
@@ -1,7 +1,8 @@
 class MemberOfAssembly < ApplicationRecord
   # acts_as_tenant(:assembly)
 
-  belongs_to :member, :assembly
+  belongs_to :member
+  belongs_to :assembly
 
   validates :year, presence: true, numericality: { only_integer: true }
   validates :member_id, uniqueness: { scope: [ :assembly_id, :year ] }
